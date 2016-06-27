561115eaa9dbfbdf376dc09c2b13a8534fe3adda
sophiebetterme <997462996@qq.com>
2016-06-27 14:19:07 +0800

    initProject

---
+++ project/project/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  project
+//
+//  Created by 袁飞霞 on 16/6/27.
+//  Copyright © 2016年 cn.sophietobeabetterone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
